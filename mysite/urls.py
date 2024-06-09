b80f2fa14a5f8558c967d783d862ef9bdb9092e6
Julio Marques <julio.jcmarques@gmail.com>
2024-06-08 23:56:24 -0300

    Criação de template basico para home page, criação de urls.py para myapp

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from myapp import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+    path('arvore/', views.arvore_genealogica, name='arvore_genealogica'),
     path('admin/', admin.site.urls),
 ]
