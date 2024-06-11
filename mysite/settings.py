692038bbdd88c36af9e886a232686d539d1bfb39
Vinicius <viniciusdaroz@gmail.com>
2024-06-10 22:53:43 -0300

    Fixing templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -115,13 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'pages/static/pages/css'),
-    os.path.join(BASE_DIR, 'familytree/static/familytree/css'),
-]
-
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
