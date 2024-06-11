708b3935d5585f6d2b005d690230de1b74847ac2
Julio Marques <julio.jcmarques@gmail.com>
2024-06-11 15:09:29 -0300

    Rebase and merge to set new main.

--- familytree/models.py
+++ familytree/models.py
@@ -1,19 +1,23 @@
 from django.db import models
-import json
+
 
 # Create your models here.
 class Person(models.Model):
     name = models.CharField(max_length=100)
     birthdate = models.DateField()
-    date_of_death = models.DateField()
+    date_of_death = models.DateField(null=True, blank=True)
     birth_place = models.CharField(max_length=100)
-    spouse = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
-    children = models.JSONField()
-    
-    # Substituir pelo código do pitz
-    def add_children(self, children_list):
-        self.children = json.dumps(children_list)
-        self.save()
+    spouse = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True, related_name='spouses'
+    )
+    parent = models.ForeignKey(
+        'self', on_delete=models.SET_NULL, null=True, blank=True, related_name='children'
+    )
 
-    def get_children(self):
-        return json.loads(self.children)
+    def __str__(self):
+        return self.name
+
+    # Substituir pelo código do pitz
+    def add_child(self, child):
+        child.parent = self
+        child.save()
--- mysite/settings.py
+++ mysite/settings.py
@@ -67,7 +67,7 @@ TEMPLATES = [{
             'django.template.context_processors.request',
             'django.contrib.auth.context_processors.auth',
             'django.contrib.messages.context_processors.messages',
-],},},]
+        ], }, },]
 
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-}}
+    }}
 
 
 # Password validation
@@ -88,13 +88,13 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [{
     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+},
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
+},
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
+},
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
 },]
