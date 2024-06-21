e899a40d4bb572ec8fa3a6842afc1d6b498fe0e2
Julio Marques <julio.jcmarques@gmail.com>
2024-06-20 23:11:56 -0300

    Adicionando arquivos faltantes

--- familytree/admin.py
+++ familytree/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Person
+from .models import Member
 
-admin.site.register(Person)
+admin.site.register(Member)
--- familytree/read_tree.py
+++ familytree/read_tree.py
@@ -1,8 +1,4 @@
-from django.db import models
-
 # Create your models here.
-
-
 class FamilyMember:
     """
     - This class handles information about family members
