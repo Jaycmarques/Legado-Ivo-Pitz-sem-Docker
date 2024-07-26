e8448270a4ff6698cc58268922c34c9c7ada6e33
Julio Marques <165230746+Jaycmarques@users.noreply.github.com>
2024-07-26 19:31:35 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -118,8 +118,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
 # URL para acessar arquivos estáticos na aplicação
 STATIC_URL = '/static/'
 
