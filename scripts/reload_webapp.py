6040438b404435543c5fad4cca3db4f6c10e40ab
Julio Marques <julio.jcmarques@gmail.com>
2024-07-28 15:42:33 -0300

    atualização script de reload da pagina

--- scripts/reload_webapp.py
+++ scripts/reload_webapp.py
@@ -1,20 +1,23 @@
 import requests
-import os
 
 # Defina suas variáveis de ambiente
 username = 'jcmarques'
-domain = 'jcmarques'
-token = os.getenv('PA_API_TOKEN')
+domain_name = 'jcmarques.pythonanywhere.com'  # Nome completo do domínio
+token = '19009bba6d7cfd7d1e6c362e3f69f6165dfab307'
+host = 'www.pythonanywhere.com'  # ou 'eu.pythonanywhere.com' se estiver na UE
 
 # URL correta para o endpoint de recarregamento
-url = f'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain}/reload/'
+url = f'https://{host}/api/v0/user/{username}/webapps/{domain_name}/reload/'
 
 # Solicitação POST para recarregar o web app
-response = requests.post(url, headers={'Authorization': f'Token {token}'})
+response = requests.post(
+    url,
+    headers={'Authorization': f'Token {token}'}
+)
 
 # Verifique a resposta
 if response.status_code == 200:
     print("Web app recarregado com sucesso!")
 else:
     print(f"Erro ao recarregar o web app: {response.status_code}")
-    print(response.text)
+    print("Resposta do servidor:", response.text)
