3639b8e8f3ecc447d1911578dc4a36582ae40a5b
Julio Marques <julio.jcmarques@gmail.com>
2024-12-01 23:59:02 -0300

    Updatings middleware

--- mysite/settings.py
+++ mysite/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'axes.middleware.AxesMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
