c4388633c6bc598a3437ed503888ad56599b57d5
Julio Marques <julio.jcmarques@gmail.com>
2025-02-16 22:47:31 -0300

    debug true para teste

--- mysite/settings.py
+++ mysite/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = bool(int(os.getenv('DEBUG', 0)))
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = [
 #     h.strip() for h in os.getenv('ALLOWED_HOSTS', '127.0.0.1, localhost').split(',') if h.strip()
