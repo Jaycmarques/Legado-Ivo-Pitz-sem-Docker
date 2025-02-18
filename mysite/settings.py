ad4c842bd48704ad621ed95f8a84a4d83f715939
Julio Marques <julio.jcmarques@gmail.com>
2025-02-17 21:00:03 -0300

    ainda tentando extrair os dados do db

--- mysite/settings.py
+++ mysite/settings.py
@@ -96,22 +96,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.config(
-#         default=os.getenv('DATABASE_URL'),
-#         conn_max_age=600,  # ou outro valor que você queira para gerenciar conexões
-#         ssl_require=True  # se seu banco exigir SSL
-#     )
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        
+    )
 }
      
  # Usando a variável já configurada no Railway
